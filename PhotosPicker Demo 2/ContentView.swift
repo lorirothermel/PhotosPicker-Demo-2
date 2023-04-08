3867d9007541f2ab57ef1e4982362713fe796e90
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-04-08 00:31:58 -0400

    PhotosPicker Demo 2

--- PhotosPicker Demo 2/ContentView.swift
+++ PhotosPicker Demo 2/ContentView.swift
@@ -6,18 +6,90 @@
 //
 
 import SwiftUI
+import PhotosUI
+
 
 struct ContentView: View {
+    
+    @State private var selectedItem: PhotosPickerItem? = nil
+    @State private var selectedImageData: Data? = nil
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        
+        ZStack {
+            
+            Color.indigo
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Spacer()
+                
+                if let selectedImageData, let image = UIImage(data: selectedImageData) {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250, height: 250)
+                }  // if let
+                
+                Spacer()
+                
+                PhotosPicker(
+                    selection: $selectedItem,
+                    matching: .images,
+                    photoLibrary: .shared()) {
+                        Label("Select a photo", systemImage: "photo")
+                    }  // PhotosPicker
+                    .tint(.cyan)
+                    .controlSize(.large)
+                    .buttonBorderShape(.roundedRectangle(radius: 10))
+                    .buttonStyle(.borderedProminent)
+                    .onChange(of: selectedItem) { newItem in
+                        Task {
+                            // Retrieve selected asset in the form of Data
+                            if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                                selectedImageData = data
+                            }  // if let data
+                        }  // Task
+                    }  // .onChange
+            }  // VStack
+            
+        }  // ZStack
+        
+        
+    }  // some View
+}  // ContentView
+
+        
+        
+        
+        
+        
+        
+        
+//        PhotosPicker(selection: $selectedItem, matching: .images, photoLibrary: .shared()) {
+//            Text("Select a Photo")
+//        }  // PhotosPicker
+//        .onChange(of: selectedItem) { newItem in
+//            Task {
+//                if let data = try? await newItem?.loadTransferable(type: Data.self) {
+//                    selectedImage = data
+//                }  // if let data
+//            }  // Task
+//        }  // .onChange
+//
+//        if let selectedImage,
+//           let uiImage = UIImage(data: selectedImage) {
+//            Image(uiImage: uiImage)
+//                .resizable()
+//                .scaledToFit()
+//                .frame(width: 300, height: 300, alignment: .center)
+//        }
+//
+//
+//    }  // some View
+//}  // ContentView
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
